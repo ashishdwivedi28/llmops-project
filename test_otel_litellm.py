7e019abd833188100dcffe39f67b9eec5b5c2cf2
Pratik Chemate <pratik.chemate@onixnet.us>
2026-03-26 17:12:42 +0530

    chore: clean up temporary test files

--- test_otel_litellm.py
+++
@@ -1,7 +0,0 @@
-import litellm
-print(litellm.success_callback)
-try:
-    litellm.success_callback = ["opentelemetry"]
-    print("Success setting opentelemetry callback")
-except Exception as e:
-    print(e)
